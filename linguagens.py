0f544b11dd340b24d3b0392be4f39d43e3196eca
meloalessandro <79473670+meloalessandro@users.noreply.github.com>
2021-02-23 12:51:48 -0300

    Update linguagens.py

--- linguagens.py
+++ linguagens.py
@@ -3,8 +3,12 @@
 
 import math
 import random
-dct={} ## Define o dicionário dct como vazio
-dct = {1: 'HTML', 2: 'BASIC', 3: 'JAVA', 4: 'COBOL', 5: 'FORTRAN', 6: 'DELPHI', 7: 'C#', 8: 'C++', 9: 'PASCAL'} ## PARA TESTES
+dct = {} ## Define o dicionário dct como vazio
+dct = {1: 'HTML', 2: 'BASIC', 3: 'JAVA', 4: 'COBOL', 5: 'FORTRAN', 6: 'DELPHI', 7: 'C#', 8: 'C++', 9: 'PASCAL'} ## Inicia o dicionário com 9 linguagens
+
+## Principal método: Pesquisa a linguagem informada pelo usuário e retorna a existência ou não de seu cadastro, informando sua chave em caso positivo
+## Caso a linguagem não seja encontrada, pergunta se o usuário deseja cadastrá-la. Em caso positivo, realiza o cadastro e informa em qual chave a linguagem
+## foi gravada.
 
 def linguagem (lg): ## lg é a váriável que contém a linguagem que está sendo pesquisada
     c=len(dct)+1 ## Inicia a váriavel c (Novo valor de CHAVE) como sendo o tamanho do dicionário+1
@@ -21,9 +25,14 @@ def linguagem (lg): ## lg é a váriável que contém a linguagem que está send
         elif conf=="N" or conf=="n" : print (lg,"não foi cadastrada")
         else: print (conf, "não é uma resposta válida. A linguagem",lg,"não foi cadastrada!")
 
+            
+## Método que efetua cadastro da linguagem no dicionário. Recebe como parâmetros ÍNDICE e LINGUAGEM
+
 def cadl (i,l):
     dct[i]=l ## cadastra a linguagem (lg) na chave (c)
     print (l,'foi cadastrada na chave',i)
+
+## Método que apresenta a opção de cadastramento de linguagem ao usuário
 
 def cadastra():
     c=len(dct)+1 ## Inicia a váriavel c (Novo valor de CHAVE) como sendo o tamanho do dicionário+1
@@ -35,6 +44,8 @@ def cadastra():
     elif conf=="N" or conf=="n" : print (cad,"não foi cadastrada")
     else: print (conf, "não é uma resposta válida. A linguagem",cad,"não foi cadastrada!")
 
+## Método que pesquisa uma linguagem no dicionário
+
 def pesquisa ():
     lg =input ('\nQual linguagem deseja pesquisar?: ')
     linguagem (lg)
@@ -42,15 +53,18 @@ def pesquisa ():
     if dnv=="S" or dnv =="s": pesquisa()
     else:
         if dnv=="N" or dnv=="n": print ("")
-            
+
+## Método que lista as linguagens cadastradas no dicionário
+
 def lista ():
     print ('Esta são as linguagens cadastradas no sistema:')
     print (dct)
 
+## Método que exibe aleatoriamente uma linguagem já cadastrada
 def aleat ():
     c=len(dct)
     r=int(random.random()*100)
-    while (r > c):
+    while (r > c): ## este laço garante que o índice 'r' gerado será menor ou igual ao número de linguagens cadastradas
         r = int(random.random()*100)
     print ('Linguagem aleatória: ',dct[r])
     
